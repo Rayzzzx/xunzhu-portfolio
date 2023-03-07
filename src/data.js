e92747fc3c771143a05aadb24484a32d19d07bec
Xun Zhu <zhu.xun1@northeastern.edu>
2023-03-06 17:19:04 -0800

    added XR project

--- src/data.js
+++ src/data.js
@@ -1,12 +1,23 @@
 import umr from "../src/images/umr.png";
+import yinyang from "../src/images/yinyang.png";
 
 export const projects = [
   {
     title: "Undefined Movies Review",
     subtitle: "MERN Stack",
     description:
-      "Website provided for users that can register/login account to search the movies they like, also log in as a reviewer can drop the score and comment for the movie.",
+      "Website provided for users that can register/login account to search the movies they like, also log in as a " +
+      "reviewer can drop the score and comment for the movie.",
     image: umr,
     link: "https://www.undefinedmoviesreview.com/",
+  },
+  {
+    title: "Yin Yang",
+    subtitle: "XR",
+    description:
+      "XR game that will provide users a platform fully immersed in digital world. Provide users two kinds of worlds, " +
+      "Yin (static) world and Yang (dynamic) world.",
+    image: yinyang,
+    link: "https://docs.google.com/document/d/1QBEr_LKFCRuTzI15x0RzG-Yb-JOpCTi5iQEHdtNbWYs/edit?usp=sharing",
   },
 ];
